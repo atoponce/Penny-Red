214884599fe06bba8deb6646f2ac784e9c9e5925
Aaron Toponce <aaron.toponce@gmail.com>
2011-03-30 15:57:47 -0600

    Added CHANGELOG, LICENSE, mint_hashcash.py and verify_hashcash.py
    Chose to use a date timestamp for the version numbers, rather than
    "1.0", "2.0", etc. This way, it is clear when the latest release is
    without checking the webpage for the date.
    
    Also, chose to use the GPLv3 for the license, because Mutt is licensed
    under the GPLv2+ and it didn't make sense to give the project a license
    that allows proprietary redistribution, when the very project it relies
    on does not.
    
    Lastly, the minting and verification Python scripts are inital adds.

---
+++ mint_hashcash.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import csv
+import fileinput
+import rfc822
+import subprocess
+import sys
+
+subprocess.call("vim %s" % sys.argv[1], shell=True)
+
+file = open(sys.argv[1], 'r')
+headers = rfc822.Message(file)
+
+to_emails = headers.getaddrlist("To")
+cc_emails = headers.getaddrlist("Cc")
+
+email_addrs = []
+tokens = []
+
+# Harvest all email addresses from the header
+for email in to_emails:
+    email_addrs.append(email[1])
+
+for email in cc_emails:
+    email_addrs.append(email[1])
+
+# Remove duplicate emails from the list, requires Python 2.5 and later
+email_addrs = list(set(email_addrs))
+
+# Check if an appropriate token is already generated for the mail
+if headers.has_key("X-Hashcash"):
+    for list in headers.getheaders("X-Hashcash"):
+        email_addrs.remove(list.split(":")[3])
+if headers.has_key("Hashcash"):
+    for list in headers.getheaders("Hashcash"):
+        email_addrs.remove(list.split(":")[3])
+
+# Call the hashcash function from the operating system to mint tokens
+for email in email_addrs:
+    t = subprocess.Popen("hashcash -m %s -X -Z 2" % email, shell=True, stdout=subprocess.PIPE)
+    tokens.append(t.stdout.read())
+
+# Write the newly minted tokens to the header
+f = fileinput.FileInput(sys.argv[1], inplace=1)
+for line in f:
+    line = line.rstrip()
+    if f.lineno() == 1:
+        for token in tokens:
+            print token,
+        print line
+        continue
+    else:
+        print line
+
+file.close()
---
+++ verify_hashcash.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# Licensed under the public domain
+
+import rfc822
+import StringIO
+import subprocess
+import sys
+
+# Change the DB path in COMMAND as needed, and change your email address
+COMMAND="hashcash -cdb '%s' -r '%s' -f ~/.mutt/hashcash.db '%s'"
+EMAILADDR="aaron.toponce@gmail.com"
+
+tokens = []
+token_status = []
+
+# converting a list to a file-type object for parsing rfc822 headers
+original = sys.stdin.read()
+emailmsg = StringIO.StringIO(''.join(original))
+message = rfc822.Message(emailmsg)
+
+# check for the presence of "X-Hashcash" and "Hashcash" headers
+if message.has_key("X-Hashcash"):
+    for hc_list in message.getheaders("X-Hashcash"):
+        tokens.append(hc_list)
+if message.has_key("Hashcash"):
+    for hc_list in message.getheaders("Hashcash"):
+        tokens.append(hc_list)
+
+# check each token
+if tokens:
+    token_status.append("[-- Begin Hashcash output --]")
+    for hc_token in tokens:
+        if hc_token.split(":")[3] == EMAILADDR:
+            hc_bits = hc_token.split(":")[1]
+            hc_resource = hc_token.split(":")[3]
+            p = subprocess.Popen(COMMAND % (hc_bits,hc_resource,hc_token),
+                shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+            out = p.stderr.read().strip()
+            token_status.append(out)
+        else:
+            token_status.append("No valid tokens for %s found." % EMAILADDR)
+    token_status.append("[-- End Hashcash output --]")
+
+print >> sys.stdout, ''.join(message.headers)
+for status in token_status:
+    print >> sys.stdout, ''.join(status)
+if tokens:
+    print ''
+emailmsg.seek(message.startofbody)
+print >> sys.stdout, ''.join(emailmsg.readlines())
