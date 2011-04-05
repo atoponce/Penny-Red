1b1c66ff00c46a9b98661a851cabb5919cec2fa2
Aaron Toponce <aaron.toponce@gmail.com>
2011-04-04 20:34:49 -0600

    Updated logic to close issues 1 & 4
    Updated logic to support the tuple of email addresses for verification. By
    doing so, not only are tokens only checked for those that have been minted
    for the addresses found en EMAILADDR, but any tokens not addressed to said
    recipients aren't checked. Also, got rid of the logic displaying that no
    tokens were found- figured it was unneeded noise.

--- verify_hashcash.py
+++ verify_hashcash.py
@@ -5,9 +5,9 @@ import StringIO
 import subprocess
 import sys
 
-# Change the DB path in COMMAND as needed, and change your email address
+# Change the DB path in COMMAND as needed, and change your email address(es)
 COMMAND="hashcash -cdb '%s' -r '%s' -f ~/.mutt/hashcash.db '%s'"
-EMAILADDR=("aaron.toponce@gmail.com")
+EMAILADDR=("foo@example.com", "bar@example.com")
 
 tokens = []
 token_status = []
@@ -18,26 +18,26 @@ emailmsg = StringIO.StringIO(''.join(original))
 message = rfc822.Message(emailmsg)
 
 # check for the presence of "X-Hashcash" and "Hashcash" headers
+# and extract only matches from EMAILADDR
 if message.has_key("X-Hashcash"):
     for hc_list in message.getheaders("X-Hashcash"):
-        tokens.append(hc_list)
+        if hc_list.split(":")[3] in EMAILADDR:
+            tokens.append(hc_list)
 if message.has_key("Hashcash"):
     for hc_list in message.getheaders("Hashcash"):
-        tokens.append(hc_list)
+        if hc_list.split(":")[3] in EMAILADDR:
+            tokens.append(hc_list)
 
 # check each token
 if tokens:
     token_status.append("[-- Begin Hashcash output --]")
-    for hc_token in tokens:
-        if hc_token.split(":")[3] in EMAILADDR:
-            hc_bits = hc_token.split(":")[1]
-            hc_resource = hc_token.split(":")[3]
-            p = subprocess.Popen(COMMAND % (hc_bits,hc_resource,hc_token),
-                shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-            out = p.stderr.read().strip()
-            token_status.append(out)
-        else:
-            token_status.append("No valid tokens for %s found." % EMAILADDR)
+    for token in tokens:
+        bits = token.split(":")[1]
+        emailaddr = token.split(":")[3]
+        p = subprocess.Popen(COMMAND % (bits, emailaddr, token),
+            shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        out = p.stderr.read().strip()
+        token_status.append(out)
     token_status.append("[-- End Hashcash output --]")
 
 print >> sys.stdout, ''.join(message.headers)
